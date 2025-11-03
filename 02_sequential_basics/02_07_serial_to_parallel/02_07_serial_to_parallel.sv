90f88410e2f7908013c5b0fe048849ef691556ae
Dafter <dafterplay@mail.ru>
2025-11-04 00:01:58 +0300

    do 02_07

--- 02_sequential_basics/02_07_serial_to_parallel/02_07_serial_to_parallel.sv
+++ 02_sequential_basics/02_07_serial_to_parallel/02_07_serial_to_parallel.sv
@@ -1,31 +1,37 @@
-//----------------------------------------------------------------------------
-// Task
-//----------------------------------------------------------------------------
+module serial_to_parallel #(
+    parameter int width = 8
+) (
+    input logic clk,
+    input logic rst,
 
-module serial_to_parallel
-# (
-    parameter width = 8
-)
-(
-    input                      clk,
-    input                      rst,
+    input logic serial_valid,
+    input logic serial_data,
 
-    input                      serial_valid,
-    input                      serial_data,
-
-    output logic               parallel_valid,
-    output logic [width - 1:0] parallel_data
+    output logic             parallel_valid,
+    output logic [width-1:0] parallel_data
 );
-    // Task:
-    // Implement a module that converts single-bit serial data to the multi-bit parallel value.
-    //
-    // The module should accept one-bit values with valid interface in a serial manner.
-    // After accumulating 'width' bits and receiving last 'serial_valid' input,
-    // the module should assert the 'parallel_valid' at the same clock cycle
-    // and output 'parallel_data' value.
-    //
-    // Note:
-    // Check the waveform diagram in the README for better understanding.
 
+  logic [$clog2(width) - 1:0] count;
+
+  always_ff @(posedge clk) begin
+    if (rst) begin
+      parallel_data  <= '0;
+      parallel_valid <= 1'b0;
+      count          <= '0;
+    end else begin
+      parallel_valid <= 1'b0;
+
+      if (serial_valid) begin
+        parallel_data <= {serial_data, parallel_data[width-1:1]};
+
+        if (count == width - 1) begin
+          parallel_valid <= 1'b1;
+          count          <= '0;
+        end else begin
+          count <= count + 1'b1;
+        end
+      end
+    end
+  end
 
 endmodule
